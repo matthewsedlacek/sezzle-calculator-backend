390a3bce198320b9f2a58d18789b162cfad84380
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-10-26 09:29:38 -0700

    updates to messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,32 @@
 class MessagesController < ApplicationController
+    def index
+        messages = Message.all
+        render json: messages
+    end
+    
+    def show
+        message = Message.find_by(id: params[:id])
+        render json: MessageSerializer.new(message)
+    end
+
+    def create
+        message = Message.create(message_params)
+        render json: MessageSerializer.new(message)
+    end
+
+    def edit
+        message = Message.update(message_params)
+        render json: MessageSerializer.new(message)
+    end
+
+    def destroy
+        message = Message.find(params[:id])
+        company.destroy
+        render json: {info: "Deleted"}
+    end
+
+    private
+    def message_params
+        params.require(:company).permit(:message_text, :username)
+    end
 end
