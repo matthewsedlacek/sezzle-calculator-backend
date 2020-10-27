144ed4ebfda2cb5c901726202501b4c24a6461a3
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-10-26 22:59:21 -0700

    changes to render json method for create messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
     include ActiveModel::Conversion
     extend  ActiveModel::Naming
-    # validates :message_text, length: { maximum: 30 }
+    validates :message_text, length: { maximum: 30 }
 end
